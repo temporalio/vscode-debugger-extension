dcb2634b39057e178a2b3fd9bef330af91622997
Anitha Rajamohan <anithaus@gmail.com>
2022-07-08 16:12:27 -0700

    add few functions to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode"
 export function activate(context: vscode.ExtensionContext): void{
 
     context.subscriptions.push(
-        vscode.commands.registerCommand("",() =>{
+        vscode.commands.registerCommand("temporal-debugger-plugin.start",() =>{
             const panel = vscode.window.createWebviewPanel(
                 "temporal-debugger-plugin",
                 "Temporal VSCode Debugger plugin",
