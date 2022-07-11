c56512e3c9cc08222ebfcf8c624570460a17f6e0
Anitha Rajamohan <anithaus@gmail.com>
2022-07-11 13:59:16 -0700

    moved the html to new file

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode"
+import { getWebViewContent as gethtml } from "./extension_HTML"
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
@@ -10,45 +11,7 @@ export function activate(context: vscode.ExtensionContext): void {
         {},
       )
       // And set its HTML content
-      panel.webview.html = getWebViewContent()
+      panel.webview.html = gethtml()
     }),
   )
-}
-
-// function deactive(){}
-
-//HTML content of the webview
-function getWebViewContent() {
-  return `<p>
-    Debug by ID
-    </p>
-    <form>
-    <input type=text required placeholder="Workflow ID *"/>
-    <input type=text placeholder="Run ID"/>
-    <input type=button value="Start" />
-    </form>
-    <hr>
-    <p>
-    Debug from history file
-    </p>
-    <form>
-    <input type=file required  />
-    <input type=button value="Start" />
-    </form>
-    <hr>
-    <p>
-    Configure server credentials (for downloading histories)
-    </p>
-    <form>
-    <label>Address</label>
-    <input type=text default="127.0.0.1:7233" />
-    <label>TLS?</label>
-    <input type=checkbox />
-    <div/>
-    <label>Client cert</label>
-    <input type=file />
-    <div/>
-    <label>Client private key</label>
-    <input type=file />
-    </form>`
 }
