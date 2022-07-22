571dcb06a8cb59228aafc4a63943254ef65e96ae
rramirez77 <rramirez77@horizon.csueastbay.edu>
2022-07-22 14:11:25 -0700

    Formatted docs

--- src/worker.ts
+++ src/worker.ts
@@ -1,14 +1,14 @@
-import { Worker } from '@temporalio/worker';
+import { Worker } from "@temporalio/worker"
 
 async function run() {
   const worker = await Worker.create({
-    workflowsPath: require.resolve('./workflows'),
-    taskQueue: 'replay-history',
-  });
-  await worker.run();
+    workflowsPath: require.resolve("./workflows"),
+    taskQueue: "replay-history",
+  })
+  await worker.run()
 }
 
 run().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+  console.error(err)
+  process.exit(1)
+})
