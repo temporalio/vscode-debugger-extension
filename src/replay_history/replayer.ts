aa6c85f0d316236c2b19260eb603013968afad0f
rramirez77 <rramirez77@horizon.csueastbay.edu>
2022-08-16 20:09:54 -0700

    HTTP Request to get History data

--- src/replay_history/replayer.ts
+++ src/replay_history/replayer.ts
@@ -1,7 +1,21 @@
+import { temporal } from "@temporalio/proto"
 import { Worker } from "@temporalio/worker"
+import http from "node:http"
 
 export async function run(): Promise<void> {
-  const history = undefined
+  const request = http.get(`${process.env.TEMPORAL_DEBUGGER_PLUGIN_URL}/history`)
+  const chunks = new Array<Buffer>()
+  const response = await new Promise<http.IncomingMessage>((resolve, reject) => {
+    request.on("response", resolve)
+    request.on("error", reject)
+  })
+
+  for await (const chunk of response) {
+    chunks.push(chunk)
+  }
+  const buffer = Buffer.concat(chunks)
+
+  const history = temporal.api.history.v1.History.decodeDelimited(buffer)
 
   // @@@SNIPSTART typescript-history-get-workflowhistory
   await Worker.runReplayHistory(
